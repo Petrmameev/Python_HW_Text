7261832fca56c9c10157ba0821d0784218864464
Petr <petr_mameev@mail.ru>
2022-12-12 23:07:28 +0700

    Task two

--- main.py
+++ main.py
@@ -18,13 +18,17 @@ def get_shop_list_by_dishes(dishes, person_count):
             # print(f'cook_book = {cook_book}')
     shop_list_by_dishes = {}
     for every_dish in dishes:
-        for every_ingredients in cook_book[every_dish]:
-            key = every_ingredients[ingredient_name]
-            quan = int(every_ingredients[quantity]) * person_count
-            mes = every_ingredients[measure]
-            shop_list_by_dishes[key] = {'quantity': quan, 'measure': mes}
+        if every_dish in cook_book.keys():
+            for every_ingredients in cook_book[every_dish]:
+                key = every_ingredients['ingredient_name']
+                if key in shop_list_by_dishes.keys():
+                    shop_list_by_dishes[key]['quantity'] += int(every_ingredients['quantity']) * person_count
+                else:
+                    mes = every_ingredients['measure']
+
+                    shop_list_by_dishes[key] = {'quantity': int(every_ingredients['quantity']) * person_count, 'measure': mes}
 
     return (shop_list_by_dishes)
 
 
-print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3))
+print(get_shop_list_by_dishes(['Омлет', 'asdfsf', 'Фахитос'], 3))
